36d5921400e9758f2daa232a2661f026dba7152c
Dmitry Demyankov <dmitry.demyankov@gmail.com>
2015-08-03 17:06:37 +0200

    added simple unit test

--- test/index.test.js
+++ test/index.test.js
@@ -1,12 +1,25 @@
-import sinon from 'sinon';
+import React from 'react/addons';
 import { expect } from 'chai';
-import reactBemGrid from './../src/index';
+import { Col } from './../src';
 
-describe('reactBemGrid', () => {
-  let sandbox;
+const TestUtils = React.addons.TestUtils;
 
-  beforeEach(() => sandbox = sinon.sandbox.create());
-  afterEach(() => sandbox.restore());
+describe('Col', () => {
+  let component;
+  
+  describe('when has text as child component', () => {
+    beforeEach(() => {
+      component = createComponent(Col, {}, 'column');
+    })
 
-  it('should reactBemGrid', () => expect(reactBemGrid('unicorns')).to.equal('unicorns'));
+    it('should have div as container element', () => expect(component.type).to.equal('div'));
+    it('should have "column" as children', () => expect(component.props.children).to.equal('column'));
+    it('should have empty className', () => expect(component.props.className).to.equal(''));
+  });
 });
+
+function createComponent(component, props, ...children) {
+  const shallowRenderer = TestUtils.createRenderer();
+  shallowRenderer.render(React.createElement(component, props, children.length > 1 ? children : children[0]));
+  return shallowRenderer.getRenderOutput();
+}
