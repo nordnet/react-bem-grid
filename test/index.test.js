a29d891d1800f7fcc3466fcb49d4b72c288c477d
Dmitry Demyankov <dmitry.demyankov@gmail.com>
2015-08-03 18:28:01 +0200

    updated unit tests, added test folder for linting

--- test/index.test.js
+++ test/index.test.js
@@ -1,25 +1,25 @@
-import React from 'react/addons';
 import { expect } from 'chai';
-import { Col } from './../src';
+import { createComponent } from './test-utils';
 
-const TestUtils = React.addons.TestUtils;
+import { Col } from './../src';
 
 describe('Col', () => {
   let component;
-  
-  describe('when has text as child component', () => {
-    beforeEach(() => {
-      component = createComponent(Col, {}, 'column');
-    })
+
+  describe('when rendered', () => {
+    beforeEach(() => component = createComponent(Col));
 
     it('should have div as container element', () => expect(component.type).to.equal('div'));
-    it('should have "column" as children', () => expect(component.props.children).to.equal('column'));
     it('should have empty className', () => expect(component.props.className).to.equal(''));
   });
-});
 
-function createComponent(component, props, ...children) {
-  const shallowRenderer = TestUtils.createRenderer();
-  shallowRenderer.render(React.createElement(component, props, children.length > 1 ? children : children[0]));
-  return shallowRenderer.getRenderOutput();
-}
+  describe('when has text as child component', () => {
+    beforeEach(() => component = createComponent(Col, {}, 'column'));
+    it('should have "column" as children', () => expect(component.props.children).to.equal('column'));
+  });
+
+  describe('when className is set via props', () => {
+    beforeEach(() => component = createComponent(Col, { className: 'app' }));
+    it('should have expected className', () => expect(component.props.className).to.equal('app'));
+  });
+});
