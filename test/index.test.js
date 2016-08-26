87a1327cb5ef6f2a9d0ba1dc45aec5533fd107a0
Andreas Broström <andreas.brostrom@gmail.com>
2016-08-26 13:36:16 +0200

    Move React and React DOM to devDeps instead of deps

--- test/index.test.js
+++ test/index.test.js
@@ -9,22 +9,28 @@ describe('Col', () => {
   describe('when rendered', () => {
     beforeEach(() => component = createComponent(Col));
 
-    it('should have div as container element', () => expect(component.type).to.equal('div'));
-    it('should have empty className', () => expect(component.props.className).to.equal(''));
+    it('should have div as container element',
+      () => expect(component.type).to.equal('div'));
+
+    it('should have empty className',
+      () => expect(component.props.className).to.equal(''));
   });
 
   describe('when has text as child component', () => {
     beforeEach(() => component = createComponent(Col, {}, 'column'));
-    it('should have "column" as children', () => expect(component.props.children).to.equal('column'));
+    it('should have "column" as children',
+      () => expect(component.props.children).to.equal('column'));
   });
 
   describe('when className is set via props', () => {
     beforeEach(() => component = createComponent(Col, { className: 'app' }));
-    it('should have expected className', () => expect(component.props.className).to.equal('app'));
+    it('should have expected className',
+      () => expect(component.props.className).to.equal('app'));
   });
 
   describe('when componentClass is set via props', () => {
     beforeEach(() => component = createComponent(Col, { componentClass: 'nav' }));
-    it('should have expected elementType', () => expect(component.type).to.equal('nav'));
+    it('should have expected elementType',
+      () => expect(component.type).to.equal('nav'));
   });
 });
